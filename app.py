63fabb48feb2bce0fecad27f3c6aa8734d30d42f
drdzcreations@gmail.com <you@example.com>
2025-05-24 23:06:24 +0530

    Initial commit for deployment

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify, render_template
+import google.generativeai as genai
+import os
+
+app = Flask(__name__)
+
+# Set your Gemini API key
+genai.configure(api_key="AIzaSyA9G5RzuVppi9-KqZMiW8ZustbASfVm34Q")
+model = genai.GenerativeModel(model_name="gemini-2.0-flash")
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get_advice", methods=["POST"])
+def get_advice():
+    data = request.get_json()
+    user_input = data["message"]
+    prompt = f"You are a relationship advisor. Give short, clear, and practical advice in 2-3 sentences. Also don't you to much vocublary, you ear vocublary\nUser: {user_input}\nBot:"
+    
+    response = model.generate_content(prompt)
+    return jsonify({"reply": response.text.strip()})
+
+if __name__ == "__main__":
+    app.run(debug=True)
